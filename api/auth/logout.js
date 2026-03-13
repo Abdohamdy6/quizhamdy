0149d63a3edce697be61350945e111295176000b
Abdohamdy6 <abdohamdy82@gmail.com>
2026-03-13 23:10:39 +0200

    Delete api directory

--- api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-import { deleteSession } from "../../lib/redis.js";
-export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).end();
-  const token = req.headers["x-auth-token"];
-  if (token) await deleteSession(token);
-  res.json({ ok: true });
-}
--- api/auth/me.js
+++
@@ -1,9 +0,0 @@
-import { getUserByToken } from "../../lib/redis.js";
-
-export default async function handler(req, res) {
-  if (req.method !== "GET") return res.status(405).end();
-  const token = req.headers["x-auth-token"] || req.query.token;
-  const user = await getUserByToken(token);
-  if (!user) return res.status(401).json({ error: "غير مسجل" });
-  res.json({ userId: user.id, displayName: user.displayName, username: user.username });
-}
